4e194dfe3282c2f1ceb6a371769afb4a59a6912c
DevPham <phamchitrong2910@gmail.com>
2026-02-25 17:46:17 +0700

    feat: Add customer module (initial)

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/tasks', function () {
     return response()->json(['status' => 'success'], 201);
 });
+
+Route::post('/customers', function () {
+    return response()->json(['message' => 'Error'], 500); 
+});
