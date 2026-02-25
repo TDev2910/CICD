0c24ebd5600a30373d9693ebc6fb4908e03e1f07
DevPham <phamchitrong2910@gmail.com>
2026-02-25 17:25:00 +0700

    This code will fail

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,5 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/tasks', function () {
-    return response()->json(['status' => 'success'], 201);
+    return response()->json(['status' => 'success'], 404);
 });
